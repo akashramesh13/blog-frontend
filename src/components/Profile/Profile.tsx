6c67f81b4c773594c038bf7eed430fa7bb77d226
akashramesh13 <akashramesh13@gmail.com>
2025-03-17 11:58:03 +0530

    change to render url

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,17 +1,20 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../../redux/reducers';
-import { getProfile } from '../../redux/actions/profileActions';
-import { ThunkDispatch } from 'redux-thunk';
-import { AnyAction } from 'redux';
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../redux/reducers";
+import { getProfile } from "../../redux/actions/profileActions";
+import { ThunkDispatch } from "redux-thunk";
+import { AnyAction } from "redux";
+import Loading from "../Loading/Loading";
 type AppDispatch = ThunkDispatch<RootState, void, AnyAction>;
 
 const Profile: React.FC = () => {
   const { profileId } = useParams<{ profileId?: string }>();
 
   const dispatch = useDispatch<AppDispatch>();
-  const { loading, id, username } = useSelector((state: RootState) => state.profile);
+  const { loading, id, username } = useSelector(
+    (state: RootState) => state.profile
+  );
   const { userInfo } = useSelector((state: RootState) => state.auth);
 
   const isOwnProfile = !profileId || profileId === userInfo?.id;
@@ -20,11 +23,11 @@ const Profile: React.FC = () => {
     dispatch(getProfile(profileId));
   }, [dispatch, profileId]);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <Loading />;
 
   return (
     <div className="profile-container">
-      <h1>{isOwnProfile ? 'My Profile' : `${username}'s Profile`}</h1>
+      <h1>{isOwnProfile ? "My Profile" : `${username}'s Profile`}</h1>
 
       <p>
         <strong>Username:</strong> {username}
--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -1,15 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:8090';
+axios.defaults.baseURL = "https://blog-backend-spring.onrender.com";
 
 axios.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+    if (
+      error.response &&
+      (error.response.status === 401 || error.response.status === 403)
+    ) {
       clearSession();
     }
     return Promise.reject(error);
-  },
+  }
 );
 
 function clearSession() {
